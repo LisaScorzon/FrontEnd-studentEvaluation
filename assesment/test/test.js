c632ac0c03e6d107964e3cf59ffdce9fc8b75a5a
LisaScorzon <lisabailey.eu@gmail.com>
2018-05-10 17:24:47 +0200

    basic mocha tests passing

--- assesment/test/test.js
+++ assesment/test/test.js
@@ -26,37 +26,19 @@ describe('my first mocha test!', function() {
         expect(1+1).to.equal(2);
     });
 });
-//expect(1+1).to.equal(2)
-
-// var randomNumber = 8
-
- 
-//   describe('A test for math.floor', function(){
- 
-//     it('Should equal 0', function (done) {
- 
-//      expect(math.floor(randomNumber) === 0).to.equal(true);
- 
-//     });
-//   });
-
-// const number = Math.floor(Math.random() * 10) 
-
-// describe('number', () => {
-//     it('should return a random number less than 10')
-//     equal(number).to.be.a('number');
-//   });
 
 
-// describe('number', function () {
-//   const test = [1,2,3,4,5,6,7,8,9]
+describe ('colorArray', function(){
+    it('should return RED evaluation if number is below 53', function(){
+        expect(20).to.be.lessThan(53);
+    })
+})
 
-//   it('always gives a random number', () => {
-//     expect(getRandomNumber(test)).toBeDefined()
-//   })
-// })
 
-// describe('number', () => {
-//       expect(x).to.be.a('number');
-//     });
- 
+// export const colorArray = (number) => {
+//     if(number <= 53) return ['RED']
+//     if(number > 53 && number <= 81) return ['YELLOW']
+//     if(number > 81) return ['GREEN']
+  
+//     console.log (colorArray(number));
+//   }
